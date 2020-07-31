a7eb6059e9adc694c8fe498f2d6e532a88f92056
WTJ <wangtianji@kanzhun.com>
2020-07-31 11:26:16 +0800

    bug

--- Package.swift
+++ Package.swift
@@ -5,11 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "SPURLNavigator",
-    dependencies: [
+    products: [
+    .library(name: "SPURLNavigator", targets: ["SPURLNavigator"])
+    ], dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url:"https://github.com/devxoul/URLNavigator.git", from: "2.3.0"),
+        .package(url:"https://github.com/devxoul/URLNavigator.git", from: "2.3.0"),
     ],
-    targets: [
+       targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
